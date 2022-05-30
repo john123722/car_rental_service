fbcc37afaa15b04dc3bfd0f863f3f20c89dc4309
john123722 <95972873+john123722@users.noreply.github.com>
2022-05-30 22:18:11 +0545

    second commit

--- carRent/rentapp/models.py
+++ carRent/rentapp/models.py
@@ -1,3 +1,17 @@
+from datetime import date
 from django.db import models
 
 # Create your models here.
+
+class Contact(models.Model):
+    name = models.CharField(max_length= 75)
+    email = models.CharField(max_length= 150)
+    contact =  models.CharField(max_length= 15)
+    reason = models.TextField()
+    date = models.DateField()
+
+    def __str__(self):
+        return self.name
+    
+
+
--- carRent/rentapp/views.py
+++ carRent/rentapp/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
-
+from datetime import datetime
+from rentapp.models import Contact
+from django.contrib import messages
 # Create your views here.
 def index(request):
     return render(request ,'index.html') 
 def contactus(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        contact = request.POST.get('contact')
+        reason = request.POST.get('reason')
+        
+        contact = Contact(name=name, email=email, contact=contact,reason = reason, date = datetime.today())
+        contact.save()
+        messages.success(request, 'Message sent successfully')
     return render(request , 'contactus.html')
 def home(request):
     return render(request, 'home.html')
